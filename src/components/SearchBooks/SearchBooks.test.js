77b5da1262fd1f31e3a3dbcd94780f02144a71c5
Jan Vigar <jansvigar@gmail.com>
2017-08-11 23:49:08 -0700

    test: add snapshot test for sortbooks

--- src/components/SearchBooks/SearchBooks.test.js
+++ src/components/SearchBooks/SearchBooks.test.js
@@ -3,10 +3,15 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import SearchBooks from './SearchBooks';
 import SearchBox from './SearchBox';
+import SortBooks from './SortBooks';
 
 const searchBoxProps = {
   handleSearch: jest.fn(),
   onClearSearch: jest.fn()
+};
+
+const sortBooksProps = {
+  handleSort: jest.fn()
 };
 
 describe('<SearchBooks />', () => {
@@ -19,6 +24,14 @@ describe('<SearchBooks />', () => {
   describe('<SearchBox />', () => {
     it('should render correctly', () => {
       const wrapper = shallow(<SearchBox {...searchBoxProps} />);
+
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+  });
+
+  describe('<SortBooks />', () => {
+    it('should render correctly', () => {
+      const wrapper = shallow(<SortBooks {...sortBooksProps} />);
 
       expect(toJson(wrapper)).toMatchSnapshot();
     });
